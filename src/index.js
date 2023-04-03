7cd8b7d88cfa7530e9263f8dcc80d26a14903f23
Ricardo-Lucena-Clear <126709276+Ricardo-Lucena-Clear@users.noreply.github.com>
2023-04-03 13:10:04 -0300

    Component with Property

--- src/index.js
+++ src/index.js
@@ -2,12 +2,20 @@ import ReactDOM  from 'react-dom';
 import React from 'react';
 import './index.css'
 import Primeiro from './components/basicos/Primeiro';
+import ComParametro from './components/basicos/ComParametro';
 
 
 ReactDOM.render(
-    <div>
+    <div id="app">
        <Primeiro></Primeiro>
-       
+       <ComParametro 
+       titulo= "Situação do Aluno" 
+       aluno= "Pedro" 
+       nota={9.3}/>
+       <ComParametro 
+       titulo= "Situação do Aluno" 
+       aluno= "Maria" 
+       nota={6.9}/>
         </div>,
     document.getElementById('root')
 );
