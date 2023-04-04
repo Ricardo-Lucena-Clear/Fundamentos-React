3ff325fc408a5a98e97e90c9be807f570d2b7aa1
Ricardo-Lucena-Clear <126709276+Ricardo-Lucena-Clear@users.noreply.github.com>
2023-04-04 10:06:08 -0300

    Random Number Challenge

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,18 @@ import React from "react";
 import Primeiro from './components/basicos/Primeiro';
 import ComParametro from './components/basicos/ComParametro';
 import Fragmento from './components/basicos/Fragmento';
+import Aleatorio from "./components/basicos/Aleatorio";
 
 
-export default () =>(
-    <div id="app">
-        <h1>Fundamentos React</h1>
+export default () => (
+<div id="app">
+    <h1>Fundamentos React</h1>
+    <Aleatorio min={1} max= {60}/>
+    <Fragmento />
 
-        <Fragmento />
+    <ComParametro titulo="Situação do Aluno" aluno="Pedro" nota={9.3} />
 
-        <ComParametro
-            titulo="Situação do Aluno"
-            aluno="Pedro"
-            nota={9.3} />
+    <Primeiro></Primeiro>
 
-        <Primeiro></Primeiro>
-
-    </div>
+</div>
 );
-
-
