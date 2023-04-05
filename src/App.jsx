1f5891c224d0f8c1e865effa05a16241a346b21a
Ricardo-Lucena-Clear <126709276+Ricardo-Lucena-Clear@users.noreply.github.com>
2023-04-05 11:38:28 -0300

    Direct Communication

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ import ListaAlunos from "./components/repetição/ListaAlunos";
 import TabelaProdutos from "./components/repetição/TabelaProdutos";
 import ParOuImpar from "./components/condicional/ParOuImpar";
 import UsuarioInfo from "./components/condicional/UsuarioInfo";
+import DiretaPai from "./components/comunicação/DiretaPai";
 
 
 export default () => (
@@ -18,6 +19,10 @@ export default () => (
         <h1>Fundamentos React</h1>
 
         <div className="Cards">
+
+            <Card titulo="#09 - Comunicação direta" color="#985239">
+                <DiretaPai></DiretaPai>
+            </Card>
 
             <Card titulo="#08 - Renderização Condicional" color="#982395">
                 <ParOuImpar numero={21}></ParOuImpar>
